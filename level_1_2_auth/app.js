9d463835bc8b62dd34a68cb8ad84fda501f41740
buzz_404 <nitkkree@gmail.com>
2023-09-21 11:18:35 +0530

    added more methods of auth

--- level_1_2_auth/app.js
+++ level_1_2_auth/app.js
@@ -4,6 +4,10 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 
 const app = express();
 
@@ -21,7 +25,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -38,28 +42,55 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-    const user = new User({
-        email: req.body.username,
-        password: req.body.password
-    })
-    user.save().then(() => {
-        res.render("secrets");
-    }).catch((err) => {
-        console.log(err);
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        //bcrypt encryption
+        bcrypt.hash(req.body.password, salt, function(err, hash) {
+            const user = new User({
+                email: req.body.username,
+                password: md5(req.body.password)
+            })
+            user.save().then(() => {
+                res.render("secrets");
+            }).catch((err) => {
+                console.log(err);
+            });
+        });
     });
+    //md5-encryption
+    // const user = new User({
+    //     email: req.body.username,
+    //     password: md5(req.body.password)
+    // })
+    // user.save().then(() => {
+    //     res.render("secrets");
+    // }).catch((err) => {
+    //     console.log(err);
+    // });
 });
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
+    // const password = md5(req.body.password); for md5
     const password = req.body.password;
 
     User.findOne({ email: username }).then((data) => {
         if (data) {
-            if (data.password === password) {
+            //md5
+            // if (data.password === password) {
+            //     res.render("secrets.ejs");
+            // } else {
+            //     console.log("Password is Wrong");
+            // }
+            bcrypt.compare(password, data.password, function(err, result) {
+                // result == true
                 res.render("secrets.ejs");
-            } else {
-                console.log("Password is Wrong");
-            }
+            });
+            bcrypt.compare(password, data.password, function(err, result) {
+                // result == false
+                console.log("Wrong Password");
+            });
+
+
         }
         if (!data) {
             console.log("User Doesn't Exist");
